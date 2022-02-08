6661e0378ce6b448e40b93f56e34ed47d3c7bb0a
Thamyris <tgthurler@gmail.com>
2022-02-08 20:40:33 -0300

    Anotações e garbage Collector

---
+++ Java Intermediario/src/com/loiane/cursojava/wrappers/test/TesteAnotacoes.java
@@ -0,0 +1,14 @@
+package com.loiane.cursojava.wrappers.test;
+
+import com.loiane.cursojava.wrappers.InformacaoAula;
+
+@InformacaoAula(autor = "Loiane", aulaNumero = 65, blog = "loiane.com")
+public class TesteAnotacoes {
+
+	@InformacaoAula(autor = "Loiane", aulaNumero = 65, blog = "loiane.com")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
