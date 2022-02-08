e7d77403173be50629c7af6b1cb59e4477d1fb44
Thamyris <tgthurler@gmail.com>
2022-02-08 19:43:32 -0300

    Escopo de variáveis

---
+++ Java Intermediario/src/com/loiane/cursojava/wrappers/EscopoVariaveis.java
@@ -0,0 +1,5 @@
+package com.loiane.cursojava.wrappers;
+
+public class EscopoVariaveis {
+
+}
---
+++ Java Intermediario/src/com/loiane/cursojava/wrappers/test/TesteVariaveis.java
@@ -0,0 +1,10 @@
+package com.loiane.cursojava.wrappers.test;
+
+public class TesteVariaveis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
