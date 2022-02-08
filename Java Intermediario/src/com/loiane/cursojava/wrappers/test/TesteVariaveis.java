0247dc0400a420706ba7c984955dac1e6f39c655
Thamyris <tgthurler@gmail.com>
2022-02-08 19:55:47 -0300

    Passagem de parâmetros por valor e por referência

--- Java Intermediario/src/com/loiane/cursojava/wrappers/test/TesteVariaveis.java
+++ Java Intermediario/src/com/loiane/cursojava/wrappers/test/TesteVariaveis.java
@@ -1,9 +1,22 @@
 package com.loiane.cursojava.wrappers.test;
 
+import com.loiane.cursojava.wrappers.EscopoVariaveis;
+
 public class TesteVariaveis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EscopoVariaveis escopo = new EscopoVariaveis();
+		escopo.setValor(10);
+
+		System.out.println(escopo.getValor()); //10
+
+		System.out.println(escopo.calculaValor(20)); //20 ou 30?
+
+		System.out.println(escopo.getValor()); //10 ou 20?
+
+		System.out.println(escopo.teste()); //4 ou 9
+
+		System.out.println(escopo.getValor());
 
 	}
 
