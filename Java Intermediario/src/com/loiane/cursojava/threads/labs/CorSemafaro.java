49a140ec18fc5aca14bc2b1ae2308a1e96a6a593
Thamyris <tgthurler@gmail.com>
2022-02-09 22:42:48 -0300

    Exercício Semáfaro com Threads

---
+++ Java Intermediario/src/com/loiane/cursojava/threads/labs/CorSemafaro.java
@@ -0,0 +1,17 @@
+package com.loiane.cursojava.threads.labs;
+
+public enum CorSemafaro {
+
+	VERDE(1000), AMARELO(300), VERMELHO(2000);
+
+	private int tempoEspera;
+
+	CorSemafaro(int tempoEspera) {
+		this.tempoEspera = tempoEspera;
+	}
+
+	public int getTempoEspera() {
+		return tempoEspera;
+	}
+
+}
