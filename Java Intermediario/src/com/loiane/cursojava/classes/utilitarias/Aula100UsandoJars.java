8d18f23af1eca1a784e4b6a7f7f49dc466dc6df1
Thamyris <tgthurler@gmail.com>
2022-02-11 20:52:50 -0300

    Utilizando jars no Projeto

---
+++ Java Intermediario/src/com/loiane/cursojava/classes/utilitarias/Aula100UsandoJars.java
@@ -0,0 +1,12 @@
+package com.loiane.cursojava.classes.utilitarias;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Aula100UsandoJars {
+
+	public static void main(String[] args) {
+		System.out.println(StringUtils.capitalize("thamyris"));
+
+	}
+
+}
